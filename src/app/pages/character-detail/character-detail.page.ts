ea001b927d67d60928eb850e873d4080b3b3ad95
Josu Adan <adan.josu92@gmail.com>
2023-05-04 16:53:57 +0200

    Rick and Morty finished app, only left upload to android and ios

---
+++ src/app/pages/character-detail/character-detail.page.ts
@@ -0,0 +1,74 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
+import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+
+@Component({
+  selector: 'app-character-detail',
+  templateUrl: './character-detail.page.html',
+  styleUrls: ['./character-detail.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, SharedModule]
+})
+export class CharacterDetailPage implements OnInit {
+
+  characterId: string = '';
+  character = null as any;
+  episodes: any[] = [];
+
+  constructor(
+    private actRoute: ActivatedRoute,
+    private rickAndMortySvc: RickAndMortyService
+  ) {
+    this.characterId = this.actRoute.snapshot.paramMap.get('id') as string
+    console.log(this.characterId)
+  }
+
+  ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    this.getCharacter();
+  }
+
+      //===== get characters
+      getCharacter() {
+
+        this.rickAndMortySvc.getCharacterById(this.characterId).subscribe({
+
+          next: (res: any) => {
+
+            this.character = res;
+            this.getEpisodes()
+
+          },
+          error: (error: any) => {
+
+          }
+        })
+      }
+
+      getEpisodes() {
+
+        for (let url of this.character.episode){
+
+          this.rickAndMortySvc.getByUrl(url).subscribe({
+
+            next: (res: any) => {
+
+              console.log(res);
+              this.episodes.push(res)
+
+            },
+            error: (error: any) => {
+
+            }
+          })
+        }
+        }
+
+
+}
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,64 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
+import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, SharedModule]
+})
+export class HomePage implements OnInit {
+
+  characters: any[] = [];
+  params = {} as any;
+
+  constructor(
+    private rickAndMortySvc: RickAndMortyService
+  ) { }
+
+  ngOnInit() {
+    this.params.page = 0;
+    this.getCharacters()
+  }
+
+    //===== get characters
+  getCharacters(event?: any) {
+    this.params.page += 1;
+
+    this.rickAndMortySvc.getCharacters(this.params).subscribe({
+
+      next: (res: any) => {
+
+        this.characters.push(...res.results)
+        console.log(this.characters);
+
+        if(event) event.target.complete();
+      },
+      error: (error: any) => {
+        if(event) event.target.complete();
+      }
+    })
+  }
+
+  //===== Search characters by name
+  searchCharacters() {
+    this.params.page = 1;
+
+    this.rickAndMortySvc.getCharacters(this.params).subscribe({
+
+      next: (res: any) => {
+
+        this.characters = res.results;
+
+      },
+      error: (error: any) => {
+      }
+    })
+  }
+
+}
